bf589a52b95ba3fe49cfece546d0fa7435b4a4ea
Mohamed-E <mohamed@themohamed.com>
2025-12-12 10:26:00 -0500

    chore: scaffold Flutter project with lints and material theme

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CargoCloudApp());
+}
+
+class CargoCloudApp extends StatelessWidget {
+  const CargoCloudApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CargoCloud',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0B2545)),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(child: Text('CargoCloud')),
+      ),
+    );
+  }
+}
